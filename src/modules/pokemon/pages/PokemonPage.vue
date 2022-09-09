ce41c050860d1463aaf55e8e28c017445ab64104
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-09-08 22:41:23 -0500

    props por router

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,13 +1,51 @@
 <template>
-  <h2>Pokemon page</h2>
+  <h2>Pokemon page <span>{{ id }}</span> </h2>
+  <!-- {{ pokemonId }} -->
+  <div v-if="pokemon">
+    <img :src="pokemon.sprites.front_default" alt="pokemon">
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      // pokemonId: this.$route.params.id
+      pokemon: null,
+    }
+  },
+  created() {
+    // const { id } = this.$route.params
+    // this.pokemonId = id
+    this.getPokemon()
+  },
+  methods: {
+    async getPokemon(){
+      try {
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`)
+                              .then( res => res.json() )
+        this.pokemon = pokemon
+      } catch (error) {
+        this.$router.push('/')
+      }
+    }
+  },
+  watch: {
+    id() {
+      this.getPokemon()
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  img{ 
+    width: 200px;
+  }
 </style>
