df581a75dd122c073d0f863019bfa2f3c7091625
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-09-08 21:40:55 -0500

    lifecycle

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,10 +1,62 @@
 <template>
   <h2>About</h2>
+  <hr>
+  <h2>{{ name }}</h2>
+  <button @click="changeName" >Cambiar nombre</button>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      name: "Kevin"
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = 'Kevin Cuadros'
+    }
+  },
+  beforeCreate(){ 
+    console.log('beforeCreate')
+  },
+  created() { 
+    //  Peticiones HTTP
+    console.log('created')
+  },
+  beforeMount() {  
+    console.log('beforeMount')
+  },
+  mounted() { 
+    console.log('mounted')
+  },
+  beforeUpdate() {  
+    console.log('beforeUpdate')
+  },
+  updated() {  
+    console.log('updated')
+  },
+  activated() {  
+    console.log('activated')
+  },
+  deactivated(){  
+    console.log('deactivated')
+  },
+  beforeUnmount() {  
+    console.log('beforeUnmount')
+  },
+  unmounted() {  
+    console.log('unmounted')
+  },
+  errorCaptured() {  
+    console.log('errorCaptured')
+  },
+  renderTracked() {  
+    console.log('renderTracked')
+  },
+  renderTriggered() { 
+    console.log('renderTriggered')
+  }
 }
 </script>
 
